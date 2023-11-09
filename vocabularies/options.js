14f8ae391576e3435629280104f149bd33fc2590
Denis Kochanov <kochanov.denis5@yandex.ru>
2023-11-09 10:47:07 +0300

    fix: Время работы;

--- vocabularies/options.js
+++ vocabularies/options.js
@@ -22,6 +22,10 @@ export const timeOptions = [
 
 export const timeList = [
   {
+    value: '10:00',
+    label: 'К 10:00'
+  },
+  {
     value: '10:30',
     label: 'К 10:30'
   },
@@ -36,6 +40,98 @@ export const timeList = [
   {
     value: '12:00',
     label: 'К 12:00'
+  },
+  {
+    value: '12:30',
+    label: 'К 12:30'
+  },
+  {
+    value: '13:00',
+    label: 'К 13:00'
+  },
+  {
+    value: '13:30',
+    label: 'К 13:30'
+  },
+  {
+    value: '14:00',
+    label: 'К 14:00'
+  },
+  {
+    value: '14:30',
+    label: 'К 14:30'
+  },
+  {
+    value: '15:00',
+    label: 'К 15:00'
+  },
+  {
+    value: '15:30',
+    label: 'К 15:30'
+  },
+  {
+    value: '16:00',
+    label: 'К 16:00'
+  },
+  {
+    value: '16:30',
+    label: 'К 16:30'
+  },
+  {
+    value: '17:00',
+    label: 'К 17:00'
+  },
+  {
+    value: '17:30',
+    label: 'К 17:30'
+  },
+  {
+    value: '18:00',
+    label: 'К 18:00'
+  },
+  {
+    value: '18:30',
+    label: 'К 18:30'
+  },
+  {
+    value: '19:00',
+    label: 'К 19:00'
+  },
+  {
+    value: '19:30',
+    label: 'К 19:30'
+  },
+  {
+    value: '20:00',
+    label: 'К 20:00'
+  },
+  {
+    value: '20:30',
+    label: 'К 20:30'
+  },
+  {
+    value: '21:00',
+    label: 'К 21:00'
+  },
+  {
+    value: '21:30',
+    label: 'К 21:30'
+  },
+  {
+    value: '22:00',
+    label: 'К 22:00'
+  },
+  {
+    value: '22:30',
+    label: 'К 22:30'
+  },
+  {
+    value: '23:00',
+    label: 'К 23:00'
+  },
+  {
+    value: '23:30',
+    label: 'К 23:30'
   }
 ]
 
